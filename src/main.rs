06a254779aec2fe36a76b10b8d0a2e47a2e75351
Rin <162780442+BanaCubed@users.noreply.github.com>
2026-07-22 13:10:45 +1200

    Basic serial monitor functionality

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,16 @@
 #![no_main]
 
 use arduino_hal::{self as hal};
-use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 
 #[hal::entry]
 fn main() -> ! {
     let dp = hal::Peripherals::take().unwrap();
     let pins = hal::pins!(dp);
-
-    let mut delay = hal::Delay::new();
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     loop {
-        delay.delay_ms(1000);
+        let _ = ufmt::uwriteln!(&mut serial, "Hello World!\r");
+        hal::delay_ms(1000);
     }
 }
